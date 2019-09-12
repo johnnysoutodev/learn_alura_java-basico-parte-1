1639ab6845f1c43fb7655e8ffbe71efba3283dee
Johnny Souto <johnny.souto@icloud.com>
2019-09-12 19:09:11 -0300

    add files of course

---
+++ sintaxe-basica/src/Programa.java
@@ -0,0 +1,8 @@
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java na Vivo.");
+	}
+
+}
---
+++ sintaxe-variaveis-fluxo/src/TestaConversao.java
@@ -0,0 +1,24 @@
+
+public class TestaConversao {
+	
+	public static void main(String[] args) {
+		
+		double salario = 1270.5;
+		
+		int valor = (int) salario;
+		
+		System.out.println(valor);
+		
+		long numeroGrande = 324324235235L;
+		
+		short valorPequeno = 32000;
+		
+		byte b = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		
+		System.out.println(valor1 + valor2);
+	}
+
+}
