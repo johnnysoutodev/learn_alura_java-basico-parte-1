ef711392f76c7b6847836b82a8ca40cf51cf42c7
Johnny Souto <johnny.souto@icloud.com>
2019-09-13 20:52:57 -0300

    Update TesteIR.java

--- sintaxe-variaveis-fluxo/src/TesteIR.java
+++ sintaxe-variaveis-fluxo/src/TesteIR.java
@@ -5,8 +5,36 @@ public class TesteIR {
 		
 		System.out.println("Testando impostos do Leão!");
 		
-		double salario = 3300.0;
+		double salario = 5400.0;
 		
+		if (salario > 4664.68) {
+			System.out.println("Sua aliquota é de 27.5%");
+			System.out.println("Você pode deduzir R$ 869,36 reais.");
+			
+		} else {
+			if(salario >= 3751.06 ) {
+				System.out.println("Sua aliquota é de 22.5%");
+				System.out.println("Você pode deduzir R$ 636,13 reais.");
+			}
+			else {
+				if(salario >= 2826.66) {
+					System.out.println("Sua aliquota é de 15%");
+					System.out.println("Você pode deduzir R$ 354,80 reais.");
+				}
+				else {
+					if(salario >= 1903.99) {
+						System.out.println("Sua aliquota é de 7,5%");
+						System.out.println("Você pode deduzir R$ 142,80 reais.");
+					}
+					else {
+						System.out.println("Você não precisa pagar imposto e não terá deduções.");
+					}
+				}
+			}
+		}
+		
+		
+		/*
 		if(salario <= 2600.0) {
 			System.out.println("A sua aliquota é de 15%");
 			System.out.println("Você pode deduzir até R$ 350");
@@ -16,6 +44,7 @@ public class TesteIR {
 			System.out.println("A sua aliquota é de 22.5%");
 			System.out.println("Você pode deduzir R$ 636");
 		}
+		*/
 	}
 
 }
