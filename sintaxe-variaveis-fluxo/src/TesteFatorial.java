8eba82417a2c7a6e20f485beffe5573ec738569c
Johnny Souto <johnny.souto@icloud.com>
2019-09-15 16:00:35 -0300

    fim do curso

---
+++ sintaxe-variaveis-fluxo/src/TesteFatorial.java
@@ -0,0 +1,12 @@
+
+public class TesteFatorial {
+	
+	public static void main(String[] args) {
+		int fatorial = 1;
+		for (int n = 1; n < 11; n++) {
+			fatorial *= n;
+			System.out.println("Fatorial de " + n + " = " +fatorial);
+		}
+	}
+
+}
